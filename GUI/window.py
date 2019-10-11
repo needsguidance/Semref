bc8576b87170f9eaaffbcbd198f8b6c198896994
Alejandro Reyes Alvarado <alejandro.reyes3@upr.edu>
2019-10-11 02:25:45 -0400

    Disabled window resizing

--- GUI/window.py
+++ GUI/window.py
@@ -11,6 +11,8 @@ from kivy.uix.popup import Popup
 from kivy.config import Config
 Config.set('graphics', 'width', '1024')
 Config.set('graphics', 'height', '650')
+Config.set('graphics', 'resizable', False)
+
 
 class TextInputPopup(Popup):
     obj = ObjectProperty(None)
