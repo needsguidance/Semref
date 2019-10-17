a09edb637d1c7911369486e830adac1199335f1e
Christian Pérez Villanueva <christian.perez34@upr.edu>
2019-10-16 21:27:29 -0400

    HEx instructions stored in list

--- microprocessor_simulator.py
+++ microprocessor_simulator.py
@@ -1,9 +1,13 @@
 class MicroSim:
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.hex_instructions = []
+
     def read_obj_file(self, filename):
         file = open(filename, 'r')
         lines = file.readlines()
         for line in lines:
-            print(line)
+            self.hex_instructions.append(line)
         lines.clear()
         file.close()
