5e1f3bebe0c733c40a5d3552381a753e075a62cd
Noel E. Valentin Roman <noel.valentin2@upr.edu>
2019-11-21 14:27:05 -0400

    Created six different .asm files, each having a different indentation problem. Created test cases that checked for the mistakes in these files. Created a test case of a file with no indentation problem.

---
+++ input/indent_test6.asm
@@ -0,0 +1,20 @@
+org 0
+JMPADDR start
+valor1 db 5
+valor2 db 7
+mayor  db 0
+const ten 0A
+
+start: LOAD R1, valor1
+    LOAD R2, valor2
+    GRT  R1, R2
+    JMPADDR R1esMayor
+STORE mayor, R2
+    JMPADDR fin
+
+R1esMayor:
+    STORE mayor, R1
+    LOADIM R3, #8
+
+fin:
+    JMPADDR fin
