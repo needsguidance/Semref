b9742ad63bc136d355655a7f2d80a921c8b551fe
Alejandro Reyes Alvarado <alejandro.reyes3@upr.edu>
2019-11-27 09:17:13 -0400

    happy hour

---
+++ input/sprt5Test5B-P14.asm
@@ -0,0 +1,30 @@
+org 0
+    JMPADDR start
+org 100
+semaforo db 0
+org 110
+sevenseg db 0
+org 120
+asciidisp1 db 0
+asciidisp2 db 0
+org 130
+keyb db 0
+org 10
+start:
+    LOADIM R5,#0A8
+    STORE semaforo,R5 
+
+    LOADIM R5,#0FF
+    STORE semaforo,R5 
+
+    LOADIM R5,#0F2
+    STORE sevenseg,R5
+    LOAD R5,#0B7
+    STORE sevenseg,R5
+
+    LOAD R5,keyb
+    LOAD R6,keyb
+    ADDIM R5,#40
+    ADDIM R6,#40
+    STORE asciidisp1,R5
+    STORE asciidisp2,R6
